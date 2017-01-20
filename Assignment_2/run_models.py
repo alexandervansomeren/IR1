20d809b52c1ea6d7edfb0640629db7ba875e9ede
iwcvanderlinden <iwc.vanderlinden@gmail.com>
2017-01-20 17:14:47 +0100

    models.py

--- Assignment_2/run_models.py
+++ Assignment_2/run_models.py
@@ -1,12 +1,23 @@
 import pyndri
 import utils
+import models
 
 def main():
+
+    # Get documents
     index = pyndri.Index('index/')
-    print("There are %d documents in this collection." % (index.maximum_document() - index.document_base()))
 
+    # Get queries
+    with open('./ap_88_89/topics_title', 'r') as f_topics:
+        topics = utils.parse_topics(f_topics)
 
+    # Construct term frequency and document frequency
+    tf = models.construct_tf(topics, index)
+    df = models.construct_df(tf)
 
+    # Run models
+    #tf_idf = models.tf_idf(tf, df)
+    #bm25 = models.bm25(tf, df)
 
 if __name__ == "__main__":
     main()
