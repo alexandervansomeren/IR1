8500d478b59aa7910d35243ca412a96dddd576f3
iwcvanderlinden <iwc.vanderlinden@gmail.com>
2017-01-21 11:54:07 +0100

    models

---
+++ Assignment_2/models.py
@@ -0,0 +1,24 @@
+
+def tf_idf(tf, df):
+    pass
+    #query_terms = collect_query_terms(topics)
+
+def bm25():
+    pass
+
+
+def construct_tf(topics, index):
+    query_terms = collect_query_terms(topics)
+    tf = np.array()
+
+def construct_df(tf):
+    pass
+
+def collect_query_terms(topics):
+    query_terms = set()
+    for query_id, query_tokens in topics.items():
+        query_id_tokens = [token2id.get(query_token,0) for query_token in query_tokens]
+        query_id_tokens = [word_id for word_id in query_id_tokens if word_id > 0]
+        query_terms |= set(query_id_tokens)
+    return query_terms
+
