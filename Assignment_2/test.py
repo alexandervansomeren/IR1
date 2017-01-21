7a5d68c8d3b39171e2af61d0af66331539c0d0ac
iwcvanderlinden <iwc.vanderlinden@gmail.com>
2017-01-21 14:04:19 +0100

    test

---
+++ Assignment_2/test.py
@@ -0,0 +1,45 @@
+import sys
+import io
+import collections
+
+def parse_topics(file_or_files,
+                 max_topics=sys.maxsize, delimiter=';'):
+    assert max_topics >= 0 or max_topics is None
+
+    topics = collections.OrderedDict()
+
+    if not isinstance(file_or_files, list) and \
+            not isinstance(file_or_files, tuple):
+        if hasattr(file_or_files, '__iter__'):
+            file_or_files = list(file_or_files)
+        else:
+            file_or_files = [file_or_files]
+
+    for f in file_or_files:
+        assert isinstance(f, io.IOBase)
+
+        for line in f:
+            assert(isinstance(line, str))
+
+            line = line.strip()
+
+            if not line:
+                continue
+
+            topic_id, terms = line.split(delimiter, 1)
+
+            if topic_id in topics and (topics[topic_id] != terms):
+                    logging.error('Duplicate topic "%s" (%s vs. %s).',
+                                  topic_id,
+                                  topics[topic_id],
+                                  terms)
+
+            topics[topic_id] = terms
+
+            if max_topics > 0 and len(topics) >= max_topics:
+                break
+
+
+with open('./ap_88_89/topics_title', 'r') as f_topics:
+    topics = utils.parse_topics(f_topics)
+
