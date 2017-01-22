9e5c873b30f8734275bd66bee8242d7ba3910f79
iwcvanderlinden <iwc.vanderlinden@gmail.com>
2017-01-22 20:39:41 +0100

    auto

--- Assignment_2/run_models.py
+++ Assignment_2/run_models.py
@@ -7,6 +7,13 @@ import json
 
 
 def main():
+
+    # File names
+    f_term_freq = 'tf.npy'
+    f_term2index = 'term2index.json'
+    f_tfidf = 'tfidf.npy'
+    f_bm25 = 'bm25.npy'
+
     # Get documents
     index = pyndri.Index('index/')
 
@@ -14,34 +21,36 @@ def main():
     with open('./ap_88_89/topics_title', 'r') as f_topics:
         topics = utils.parse_topics(f_topics)
 
-    tf_filename = 'tf.npy'
-    term2index_filename = 'term2index.json'
-    # Construct term frequency and document frequency
-    if os.path.isfile(tf_filename) and os.path.isfile(term2index_filename):
-        with open(tf_filename, 'rb') as f:
+    # Construct term frequency matrix
+    if os.path.isfile(f_term_freq) and os.path.isfile(f_term2index):
+        with open(f_term_freq, 'rb') as f:
             tf = np.load(f)
-        with open(term2index_filename, 'r') as f:
+        with open(f_term2index, 'r') as f:
             term2index = json.load(f)
     else:
         tf, term2index = models.construct_tf(topics, index, max_query_terms=0, max_documents=0)
-        with open(tf_filename, 'wb') as f:
+        with open(f_term_freq, 'wb') as f:
             np.save(f, tf)
-        with open(term2index_filename, 'w') as f:
+        with open(f_term2index, 'w') as f:
             json.dump(term2index, f)
 
+    # Construct document frequency vector
     df = (tf > 0).sum(axis=1)
+    # TODO
     if 0 in df:
         df += 1        
     idf = np.log(float(tf.shape[1]) / df)
 
     # Run models
-    #tf_idf = models.tf_idf(tf, idf)
-    bm25 = models.bm25(tf, df, 1.2, 0.75)
+    if not os.path.isfile(f_tfidf):
+        tf_idf = models.tf_idf(tf, idf)
+        with open('tfidf.npy', 'wb') as f:
+            np.save(f, tf_idf)
 
-    #with open('tfidf.npy', 'wb') as f:
-    #    np.save(f, tf_idf)
-    with open('bm25.npy', 'wb') as f:
-        np.save(f, bm25)
+    if not os.path.isfile(f_bm25):
+        bm25 = models.bm25(tf, idf, 1.2, 0.75)
+        with open('bm25.npy', 'wb') as f:
+            np.save(f, bm25)
 
 if __name__ == "__main__":
     main()
