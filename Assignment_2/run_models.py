ef1e2d42a3afeae85deee7ce1a4d907026165be9
alexandervansomeren <alexander.vansomeren@gmail.com>
2017-01-22 13:13:36 +0100

    Development commit (for virtual machine..)

--- Assignment_2/run_models.py
+++ Assignment_2/run_models.py
@@ -11,14 +11,14 @@ def main():
     index = pyndri.Index('index/')
 
     # Get queries
-    with open('./ap_88_89/topics_title', 'rb') as f_topics:
+    with open('./ap_88_89/topics_title', 'r') as f_topics:
         topics = utils.parse_topics(f_topics)
 
     tf_filename = 'tf.npy'
 
     # Construct term frequency and document frequency
     if os.path.isfile(tf_filename):
-        with open(tf_filename, 'r') as f:
+        with open(tf_filename, 'rb') as f:
             tf = np.load(f)
     else:
         tf = models.construct_tf(topics, index, max_query_terms=10, max_documents=10)
