e4ff9a06630368123e9d312781ffb03bfd16558c
iwcvanderlinden <iwc.vanderlinden@gmail.com>
2017-01-22 20:15:32 +0100

    auto

--- Assignment_2/models.py
+++ Assignment_2/models.py
@@ -8,9 +8,9 @@ def tf_idf_score(tf_idf, query_indices):
     return tf_idf[query_indices[0:None], :].sum(axis=0)
 
 def bm25(tf, idf, k, b):
-    doc_normalization = tf.sum(axis=0) / (float(tf.sum() / float(tf.shape[1])))
+    doc_normalization = np.divide(np.sum(tf, axis=0), np.average(np.sum(tf,axis=0)))
     #return (np.divide(((k+1) * tf), (k * ((1-b) + b * doc_normalization) + tf)).T * idf.T).T
-    return (np.divide(((k+1) * tf), (k+tf)).T * idf.T).T
+    return (np.divide(((k+1) * tf), (k * ((1-b) + b * doc_normalization) +tf)).T * idf.T).T
 
 
 def bm25_score(bm25, query_indices): 
