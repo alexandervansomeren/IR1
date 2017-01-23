1acf1c308ae41c5d7dbbce7e05227be5998d87d1
alexandervansomeren <alexander.vansomeren@gmail.com>
2017-01-23 14:38:03 +0100

    Development commit (for virtual machine..)

--- Assignment_2/models.py
+++ Assignment_2/models.py
@@ -1,35 +1,47 @@
 import numpy as np
+from collections import Counter
 
 
 def tf_idf(tf, idf):
     return (np.log(1 + tf).T * idf.T).T  # tf-idf
 
+
 def tf_idf_score(tf_idf, query_indices):
     return tf_idf[query_indices[0:None], :].sum(axis=0)
 
+
+def tf_idf_score_cosine(tf_idf, query_indices, df):
+    query_counts_per_index = Counter(query_indices)
+    query_tf = np.array(query_counts_per_index.values())
+    query_tf_idf = np.log(1+query_tf) * (float(tf_idf.shape[1]) / df[query_counts_per_index.keys()[0:None], :])
+    return cosine_similarity(tf_idf[query_counts_per_index.keys()[0:None], :], query_tf_idf)
+
+
 def bm25(tf, idf, k, b):
-    doc_normalization = np.divide(np.sum(tf, axis=0), np.average(np.sum(tf,axis=0)))
-    #return (np.divide(((k+1) * tf), (k * ((1-b) + b * doc_normalization) + tf)).T * idf.T).T
-    return (np.divide(((k+1) * tf), (k * ((1-b) + b * doc_normalization) +tf)).T * idf.T).T
+    doc_normalization = np.divide(np.sum(tf, axis=0), np.average(np.sum(tf, axis=0)))
+    # return (np.divide(((k+1) * tf), (k * ((1-b) + b * doc_normalization) + tf)).T * idf.T).T
+    return (np.divide(((k + 1) * tf), (k * ((1 - b) + b * doc_normalization) + tf)).T * idf.T).T
 
 
-def bm25_score(bm25, query_indices): 
-    return bm25[query_indices[0:None], :].sum(axis=0) 
+def bm25_score(bm25, query_indices):
+    return bm25[query_indices[0:None], :].sum(axis=0)
+
 
 def query2indices(query, term2index):
-    query_indices = []    
+    query_indices = []
     query_tokens = query.lower().split(' ')
     for term in query_tokens:
-        if term in term2index: #TODO why (lending)?
+        if term in term2index:  # TODO why (lending)?
             term_index = term2index[term]
             query_indices.append(term_index)
     return query_indices
+
 
 def construct_tf(topics, index, max_query_terms=0, max_documents=0):
     token2id, id2token, _ = index.get_dictionary()
     query_term_ids = collect_query_terms(topics, token2id)
 
-    n_docs = index.document_count() 
+    n_docs = index.document_count()
     # Take small sample for debugging purposes
     if max_query_terms > 0:
         query_term_ids = list(query_term_ids)[0:max_query_terms]
@@ -46,6 +58,7 @@ def construct_tf(topics, index, max_query_terms=0, max_documents=0):
     # Create dictionary to retrieve index of term in tf matrix
     term2index = {id2token[term_id]: index for index, term_id in enumerate(query_term_ids)}
     return tf, term2index
+
 
 def collect_query_terms(topics, token2id):
     query_term_ids = set()
@@ -65,4 +78,3 @@ def cosine_similarity(tf_idf, query):
     :return: numpy array of cosine similarity per document
     """
     return np.einsum('ij,i->j', tf_idf, query) / np.linalg.norm(tf_idf, axis=0) * np.linalg.norm(query)
-
