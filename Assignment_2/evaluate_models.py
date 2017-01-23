9db0a32b85fff24d1d7f91d9a213f4f829322985
alexandervansomeren <alexander.vansomeren@gmail.com>
2017-01-23 20:37:50 +0100

    Development commit (for virtual machine..)

--- Assignment_2/evaluate_models.py
+++ Assignment_2/evaluate_models.py
@@ -13,12 +13,7 @@ def main():
     with open('./ap_88_89/topics_title', 'r') as f_topics:
         topics = utils.parse_topics(f_topics)
 
-    # Get models
-    with open('tfidf.npy', 'rb') as f:
-        tf_idf = np.load(f)
-    with open('bm25.npy', 'rb') as f:
-        bm25 = np.load(f)
-    # Get dictionary mapping terms to model index    
+    # Get dictionary mapping terms to model index
     with open('term2index.json', 'r') as f:
         term2index = json.load(f)
 
@@ -26,19 +21,44 @@ def main():
     for i in range(index.document_base(), index.maximum_document()):
         doc_names.append(index.document(i)[0])
 
+    """
+    TF IDF
+    """
+    print("Evaluating tf_idf")
     tfidf_results = {}
-    bm25_results = {}
+    with open('tfidf.npy', 'rb') as f:
+        tf_idf = np.load(f)
+
     for query_id, query in topics.items():
         query_indices = models.query2indices(query, term2index)
         tfidf_score = models.tf_idf_score(tf_idf, query_indices)
-        bm25_score = models.bm25_score(bm25, query_indices)
+        print("   Score: " + str(tfidf_score) +  "   Query: " + query)
         tfidf_results[query_id] = list(zip(tfidf_score, doc_names))
-        bm25_results[query_id] = list(zip(bm25_score, doc_names))
 
     utils.write_run(model_name='tfidf', data=tfidf_results,
                     out_f='results/ranking_tfidf.txt', max_objects_per_query=1000)
+
+    del tf_idf
+    del tfidf_results
+
+    with open('bm25.npy', 'rb') as f:
+        bm25 = np.load(f)
+
+    """
+    BM25
+    """
+    print("Evaluating bm25")
+    bm25_results = {}
+    for query_id, query in topics.items():
+        query_indices = models.query2indices(query, term2index)
+        bm25_score = models.bm25_score(bm25, query_indices)
+        bm25_results[query_id] = list(zip(bm25_score, doc_names))
+
     utils.write_run(model_name='bm25', data=bm25_results,
                     out_f='results/ranking_bm25.txt', max_objects_per_query=1000)
+
+    del tf_idf
+    del tfidf_results
 
 
 # trec_eval -m all_trec -q ap_88_89/qrel_validation ranking_tfidf.txt | grep -E "^map\s"
