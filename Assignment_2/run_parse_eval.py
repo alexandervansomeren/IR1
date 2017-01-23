39e82ec260586f29f3b57baa7b49c833f4bdee57
alexandervansomeren <alexander.vansomeren@gmail.com>
2017-01-23 18:29:10 +0100

    Development commit (for virtual machine..)

--- Assignment_2/run_parse_eval.py
+++ Assignment_2/run_parse_eval.py
@@ -5,7 +5,7 @@ import pandas as pd
 for result_file in glob.glob('./results/*.txt'):
     subprocess.call(
         'trec_eval -m all_trec -q ap_88_89/qrel_test '
-        + result_file + ' | grep -E "(^ndcg_cut_10)|(^P_5)|(^recall_1000)|(^map_cut_1000)" > ./test/' +
+        + result_file + ' | grep -E "(^ndcg_cut_10)|(^P_5)|(^recall_1000)|(^map_cut_1000)" > ./trec_results/' +
         result_file.split('/')[-1], shell=True)
 
 # for trec_result_file in glob.glob('./test/*.txt'):
