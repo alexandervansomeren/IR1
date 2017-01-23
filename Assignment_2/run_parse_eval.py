1ab3742a26ff0d3feb4e2c85f4acdd4ba1caebc0
alexandervansomeren <alexander.vansomeren@gmail.com>
2017-01-23 18:54:29 +0100

    Development commit (for virtual machine..)

--- Assignment_2/run_parse_eval.py
+++ Assignment_2/run_parse_eval.py
@@ -1,4 +1,5 @@
 import glob
+import pickle
 import subprocess
 import pandas as pd
 
@@ -8,6 +9,15 @@ for result_file in glob.glob('./results/*.txt'):
         + result_file + ' | grep -E "(^ndcg_cut_10)|(^P_5)|(^recall_1000)|(^map_cut_1000)" > ./trec_results/' +
         result_file.split('/')[-1], shell=True)
 
-# for trec_result_file in glob.glob('./test/*.txt'):
-#     with open(trec_result_file, 'r'):
-#         pass
+results = []
+for trec_result_file in glob.glob('./trec_results/*.txt'):
+    with open(trec_result_file, 'r') as f:
+        df = pd.read_csv(trec_result_file, header=None, delimiter=r"\s+")
+        df = df.pivot(index=1, columns=0, values=2)
+        model_name = trec_result_file.split('_')[-1][0:-4] + '_'
+        df.columns = [model_name + col_name for col_name in df.columns]
+        results.append(df.copy())
+
+results = pd.concat(results, axis=1)
+with open('results.pickle', 'wb') as f:
+    pickle.dump(results, f)
