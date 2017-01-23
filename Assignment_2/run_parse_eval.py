ecc8c5323a56827716ac7c3b213e8ecb0b2d32c0
alexandervansomeren <alexander.vansomeren@gmail.com>
2017-01-23 18:27:45 +0100

    Development commit (for virtual machine..)

--- Assignment_2/run_parse_eval.py
+++ Assignment_2/run_parse_eval.py
@@ -6,8 +6,8 @@ for result_file in glob.glob('./results/*.txt'):
     subprocess.call(
         'trec_eval -m all_trec -q ap_88_89/qrel_test '
         + result_file + ' | grep -E "(^ndcg_cut_10)|(^P_5)|(^recall_1000)|(^map_cut_1000)" > ./test/' +
-        result_file.split('/')[-1])
+        result_file.split('/')[-1], shell=True)
 
-for trec_result_file in glob.glob('./test/*.txt'):
-    with open(trec_result_file, 'r'):
-        pass
+# for trec_result_file in glob.glob('./test/*.txt'):
+#     with open(trec_result_file, 'r'):
+#         pass
