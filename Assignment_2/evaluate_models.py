c0df376debeed584c75109771280b3781fea50f6
alexandervansomeren <alexander.vansomeren@gmail.com>
2017-01-23 20:27:13 +0100

    Development commit (for virtual machine..)

--- Assignment_2/evaluate_models.py
+++ Assignment_2/evaluate_models.py
@@ -7,7 +7,6 @@ import json
 
 
 def main():
-
     # Get documents
     index = pyndri.Index('index/')
     # Get queries
@@ -29,17 +28,18 @@ def main():
 
     tfidf_results = {}
     bm25_results = {}
-    for query_id, query in topics.items():    
+    for query_id, query in topics.items():
         query_indices = models.query2indices(query, term2index)
         tfidf_score = models.tf_idf_score(tf_idf, query_indices)
         bm25_score = models.bm25_score(bm25, query_indices)
         tfidf_results[query_id] = list(zip(tfidf_score, doc_names))
         bm25_results[query_id] = list(zip(bm25_score, doc_names))
 
-    utils.write_run(model_name='tfidf', data=tfidf_results, 
-                    out_f='ranking_tfidf.txt', max_objects_per_query=1000)
+    utils.write_run(model_name='tfidf', data=tfidf_results,
+                    out_f='results/ranking_tfidf.txt', max_objects_per_query=1000)
     utils.write_run(model_name='bm25', data=bm25_results,
-                    out_f='ranking_bm25.txt', max_objects_per_query=1000)
+                    out_f='results/ranking_bm25.txt', max_objects_per_query=1000)
+
 
 # trec_eval -m all_trec -q ap_88_89/qrel_validation ranking_tfidf.txt | grep -E "^map\s"
 # trec_eval -m all_trec -q ap_88_89/qrel_validation ranking_bm25.txt | grep -E "^map\s"
