884e80d06137e47ceb6a4bfa46099f208f88673e
alexandervansomeren <alexander.vansomeren@gmail.com>
2017-01-24 16:21:35 +0100

    Development commit (for virtual machine..)

--- Assignment_2/run_sig_tests.py
+++ Assignment_2/run_sig_tests.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('results_task1.csv')
+df = pd.read_csv('results_eval_task1.csv')
 df.index = df['1']
 df.drop('1', axis=1, inplace=True)
 measure_names = ['ndcg_cut_10', 'map_cut_1000','P_cut_5', 'recall_cut_1000']
