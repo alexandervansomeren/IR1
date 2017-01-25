f084c74d4956304295f3bc0323bfb0b27672f489
iwcvanderlinden <iwc.vanderlinden@gmail.com>
2017-01-25 02:15:56 +0100

    task2

--- Assignment_2/lsm_retrieval.py
+++ Assignment_2/lsm_retrieval.py
@@ -24,10 +24,10 @@ def main():
     print("Building document representations")
     doc_representations = np.zeros([embedding_size, index.document_count()])        
     for d in range(index.document_base(), index.maximum_document()):
-        doc_tokens = index.document(d)[1]
+        doc = index.document(d)[1]
         docvec = np.zeros([embedding_size, len(doc_tokens)])
-        for i, token in enumerate(doc_tokens):
-            word = token2id.get(token,0)
+        for i, word_id in enumerate(doc):
+            word = id2token.get(word_id,0)
             docvec[:,i] = model[word]
         doc_representations[:,d] = np.mean(docvec, axis=1)
 
