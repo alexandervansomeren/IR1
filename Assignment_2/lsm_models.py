449d5d69d9956261b9603bbaf86c12115885a939
iwcvanderlinden <iwc.vanderlinden@gmail.com>
2017-01-25 16:38:24 +0100

    task2

--- Assignment_2/lsm_models.py
+++ Assignment_2/lsm_models.py
@@ -9,37 +9,49 @@ import connector_classes
 
 class Word2Vec():
 
-    def __init__(self, embedding_size=300):
-        self.model = gensim.models.Word2Vec(
-            size=embedding_size,  # Embedding size
-            window=5,  # One-sided window size
-            sg=True,  # Skip-gram. (False for CBOW)
-            min_count=5,  # Minimum word frequency.
-            sample=1e-3,  # Sub-sample threshold.
-            hs=False,  # Hierarchical softmax.
-            negative=10,  # Number of negative examples.
-            iter=1,  # Number of iterations.
-            workers=8)  # Number of workers.
+    def __init__(self, filename=None, embedding_size=300, max_documents=500):
+        self.embedding_size=embedding_size
+        self.max_documents=max_documents
+        if filename == None:
+            self.model = gensim.models.Word2Vec(
+                size=self.embedding_size,  # Embedding size
+                window=5,  # One-sided window size
+                sg=True,  # Skip-gram. (False for CBOW)
+                min_count=5,  # Minimum word frequency.
+                sample=1e-3,  # Sub-sample threshold.
+                hs=False,  # Hierarchical softmax.
+                negative=10,  # Number of negative examples.
+                iter=1,  # Number of iterations.
+                workers=8)  # Number of workers.
+        else:
+            self.model = gensim.models.Word2Vec.load(filename)
 
     def train(self, index):  
         dictionary = pyndri.extract_dictionary(index)
-        sentences = connector_classes.IndriSentences(index, dictionary)
+        sentences = connector_classes.IndriSentences(index, dictionary, 
+                                                     max_documents=self.max_documents)
         self.model.build_vocab(sentences, trim_rule=None)
         self.model.train(sentences)
     
     def docs2vec(self, index):
-        doc_representations = np.zeros([embedding_size, index.document_count()])        
-        for d in range(index.document_base(), index.maximum_document()):
-            doc = index.document(d)[1]
-            docvec = np.zeros([embedding_size, len(doc)])
-            for i, word_id in enumerate(doc):
-                word = id2token.get(word_id,0)
-                docvec[:,i] = model[word]
-            doc_representations[:,d] = np.mean(docvec, axis=1)
-        return doc_representations
+        #docs_representation = np.zeros([embedding_size, index.document_count()])        
+        docs_representation = np.zeros([self.embedding_size,self.max_documents])        
+        for d in range(self.max_documents):#index.maximum_document()):
+            doc = index.document(d+1)[1]
+            doc_words = list((id2token.get(word_id) for word_id in doc if word_id != 0))
+            docvec = np.zeros([self.embedding_size, len(doc_words)])
+            for i, word in enumerate(doc_words):
+                docvec[:,i] = self.model[word]
+            docs_representation[:,d] = np.mean(docvec, axis=1)
+        return docs_representation
 
+    # Build query representation as average of wordvectors
     def query2vec(self, query):
-        pass
+        query_tokens = query.lower().split(' ')
+        wordvec = np.zeros([self.embedding_size, len(query_tokens)])
+        for i, word in enumerate(query_tokens):
+            wordvec[:,i] = self.model[word]
+        return np.mean(wordvec, axis=1)
 
     def rank(self):
         pass
--- Assignment_2/lsm_retrieval.py
+++ Assignment_2/lsm_retrieval.py
@@ -1,3 +1,4 @@
+import os
 import pyndri
 import gensim
 import utils
@@ -14,35 +15,56 @@ def main():
     with open('./ap_88_89/topics_title', 'r') as f_topics:
         topics = utils.parse_topics(f_topics)
 
-    print("Building word2vec")
+    initialize_folders()
+
+    print("Building / loading word2vec")
     embedding_size = 50
-    #word2vec_model = lsm_models.word2vec_model(index, embedding_size)
-    model = gensim.models.Word2Vec.load('models/word2vec.model')
+    max_documents = 500
+    wv2_model_filename = 'models/word2vec.model'    
+    if os.path.isfile(wv2_model_filename):
+        w2v = lsm_models.Word2Vec(filename=wv2_model_filename,
+                                  embedding_size=embedding_size,
+                                  max_documents=max_documents)
+    else:
+        w2v = lsm_models.Word2Vec() 
+        wv2.train(index)
+    #w2v_model = lsm_models.word2vec_model(index, embedding_size)
+    #w2v_model = gensim.models.Word2Vec.load('models/word2vec.model')
+    print('Size Word2Vec model')
+    print(len(w2v.model))
    
-    # TODO replace block by:
-    # doc_representations = word2vec_model.docs2vec(index)
     print("Building document representations")
-    doc_representations = np.zeros([embedding_size, index.document_count()])        
-    for d in range(index.document_base(), index.maximum_document()):
-        doc = index.document(d)[1]
-        doc_words = list((id2token.get(word_id) for word_id in doc if(word_id != 0 and id2token.get(word_id) in model.vocab)))
-        docvec = np.zeros([embedding_size, len(doc_words)])
-        for i, word in enumerate(doc_words):
-            docvec[:,i] = model[word]
-        doc_representations[:,d] = np.mean(docvec, axis=1)
+    docs_representation_filename = 'tmp/doc2vecs.npy')
+    if os.path.isfile(docs_representation_filename):
+        docs_representation = w2v.docs2vec(index)
+        with open(docs_representation_filename, 'wb') as f:
+            np.save(f, docs_representation)
+    else:
+        with open(docs_representation_filename, 'rb') as f:
+            docs_representation = np.load(f)
+#     doc_representations = np.zeros([embedding_size, index.document_count()])        
+#     for d in range(index.document_base(), index.maximum_document()):
+#         doc = index.document(d)[1]
+#         doc_words = list((id2token.get(word_id) for word_id in doc if(word_id != 0 and id2token.get(word_id) in model.vocab)))
+#         docvec = np.zeros([embedding_size, len(doc_words)])
+#         for i, word in enumerate(doc_words):
+#             docvec[:,i] = model[word]
+#         doc_representations[:,d] = np.mean(docvec, axis=1)
 
     print("Evaluating word2vec")
     word2vec_results = {}
     for query_id, query in topics.items():
+        query_representation = w2v.query2vec(query)
         # Build query representation as average of wordvectors
-        query_tokens = query.lower().split(' ')
-        wordvec = np.zeros([embedding_size, len(query_tokens)])
-        for i, word in enumerate(query_tokens):
-            wordvec[:,i] = model[word]
-        query_representation = np.mean(wordvec, axis=1)
+        #query_tokens = query.lower().split(' ')
+        #wordvec = np.zeros([embedding_size, len(query_tokens)])
+        #for i, word in enumerate(query_tokens):
+        #    wordvec[:,i] = model[word]
+        #query_representation = np.mean(wordvec, axis=1)
+
         # Calculate the similarity with documents
-        similarity = np.zeros([index.document_count()])
-        for d in range(index.document_count()):
+        similarity = np.zeros([max_documents])
+        for d in range(max_documents):
             similarity[d] = cosine_similarity(query_representation, doc_representation[:,d])
 
         print(query)
@@ -53,6 +75,12 @@ def main():
         print(line)
         #word2vec_results[query_id] = simila
         
+
+def initialize_folders():
+    if not os.path.exists('models'):
+        os.makedirs('models')
+    if not os.path.exists('tmp'):
+        os.makedirs('tmp')
 
 if __name__ == "__main__":
     main()
