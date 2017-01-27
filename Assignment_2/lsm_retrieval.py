5436e9b86318cd3dfdf8474ccb559d26d1122bba
iwcvanderlinden <iwc.vanderlinden@gmail.com>
2017-01-27 02:32:01 +0100

    all

--- Assignment_2/lsm_retrieval.py
+++ Assignment_2/lsm_retrieval.py
@@ -234,7 +234,7 @@ if __name__ == "__main__":
     # Command line arguments
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--method", type=str, default='lda',
+    parser.add_argument("--method", type=str, default='word2vec',
                         help='Latent semanctic model [word2vec, lsi, lda, doc2vec].')
 
     FLAGS = parser.parse_args()
