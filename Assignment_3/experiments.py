d0efdf82632bb6d019376f21d8bd0cb54d052d37
iwcvanderlinden <iwc.vanderlinden@gmail.com>
2017-01-31 12:55:15 +0100

    Assignment 3

---
+++ Assignment_3/experiments.py
@@ -0,0 +1,37 @@
+import query
+import document
+import LambdaRankHW
+
+FLAGS = None
+
+def main():
+    
+    number_of_features = 64
+    number_of_epochs = 1
+    algorithm = FLAGS.method
+
+    for fold in range(1): #5
+        fold_dir = 'HP2003/Fold' + str(fold+1) 
+        train_queries = query.load_queries(fold_dir + '/train.txt', number_of_features)
+
+        model = LambdaRankHW(algorithm, number_of_features)
+        model.train_with_queries(train_queries, num_of_epochs)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    # Command line arguments
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--method", type=str, default='pointwise',
+                        help='Learning to rank method [pointwise, pairwise, lambdarank].')
+
+    FLAGS = parser.parse_args()
+
+    main()
+
+    #if FLAGS.method
